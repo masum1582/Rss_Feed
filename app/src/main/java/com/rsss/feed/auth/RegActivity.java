30249eeec95c4f2fd9ea1c55f629a49721066327
masum1582 <maasum101@gmail.com>
2020-06-20 15:03:42 +0600

    Initial commit

---
+++ app/src/main/java/com/rsss/feed/auth/RegActivity.java
@@ -0,0 +1,16 @@
+package com.rsss.feed.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rsss.feed.R;
+
+public class RegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+    }
+}
